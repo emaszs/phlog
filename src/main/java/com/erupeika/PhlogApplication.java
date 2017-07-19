2e2f32014fa0b283bc1116f2791f2f95d93d1624
Emilis Rupeika <phonixreal@gmail.com>
2017-07-20 00:34:17 +0200

    init

---
+++ src/main/java/com/erupeika/PhlogApplication.java
@@ -0,0 +1,12 @@
+package com.erupeika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhlogApplication.class, args);
+	}
+}
